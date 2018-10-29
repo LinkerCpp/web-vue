a2ba6cb9db4c65bdee108570b893a1dd58f41214
Willian Amaral <willian.amaral@outlook.com.br>
2018-10-29 20:34:17 -0300

    Inicializar repositório

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue App
+
+App construído com VueJS para a disciplina de Desenvolvimento Web I do curso de Análise e Desenvolvimento 
+de Sistemas do IFSP - Campos do Jordão. 
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {loader: 'babel-loader'}
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+    	template: './public/index.html'
+    })
+  ]
+}
